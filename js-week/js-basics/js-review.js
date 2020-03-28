0c49ebcc0e562bb5cdd18617d2da31ca2694d4c1
Marcel Fonseca <marcelsf23br@gmail.com>
2020-03-28 10:33:17 -0300

    review done

--- js-week/js-basics/js-review.js
+++ js-week/js-basics/js-review.js
@@ -1,30 +1,73 @@
 // Small review for Batch #354
 
 // How to print something?
-
+console.log("hello world!");
 
 // how to see the type of something?
-
+console.log(typeof(42));
 
 // How to change data types?
+console.log((42).toString());
 
+console.log(Number.parseInt("42", 10));
 
 // let or const?
 
 
+const pi = 3.14
+
+let myAge = 32
+
 // Defining an Array
 
+const bandMembers = ['Jimmy Page', 'John Bonham', 'John Paul Jones', 'Robert Plant']
 
-// How to use the Each loop?
+// console.log(bandMembers);
+// // How to use the Each loop?
 
-
+// bandMembers.forEach((member) => {
+//   console.log(`${member} is an integral part of Led Zeppelin`);
+// });
 // Defining an Object
-
+const student = {
+  firstName: "Andre",
+  lastName: "Ferrer"
+}
 
 // Reading / setting properties of an Object
-
-
+student.lastName = "Romeiro";
+console.log(student.lastName);
 // Defining a function
-
+const square = (number) => {
+  return number * number;
+}
+// Calling a function
+console.log(square(4));
 
 // Logical flow example:
+const quarantine = true;
+
+if (quarantine === true) {
+  console.log("Stay home!");
+} else {
+  console.log("You can leave the house!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
